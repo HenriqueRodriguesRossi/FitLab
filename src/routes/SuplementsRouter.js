7946d3cca76682ae50ced8f29354141d6fa51d04
HenriqueRodriguesRossi <hrodriguescruciakrossi46@gmail.com>
2023-10-29 23:47:11 -0300

    Planejando as rotas relacionadas a produtos

--- src/routes/SuplementsRouter.js
+++ src/routes/SuplementsRouter.js
@@ -5,4 +5,13 @@ const SuplementController = require("../controllers/SuplementController")
 
 router.post("/suplements/new/:store_id", checkStoreToken, upload.single("file"), SuplementController.newSuplement)
 
+router.get("/suplements/find/all/:store_id", checkStoreToken,)
+router.get("/suplements/find/name",checkStoreToken,)
+router.get("/suplements/find/quantity", checkStoreToken,)
+
+router.put("/suplements/alter/quantity_stock", checkStoreToken)
+router.put("/suplements/alter/unit_value", checkStoreToken,)
+
+router.delete("/suplements/delete/:suplement_id", checkStoreToken)
+
 module.exports = router
